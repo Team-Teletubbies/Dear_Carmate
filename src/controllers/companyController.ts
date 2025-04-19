a0be5a80ff47c005c3110d6df2d04b08eb7d055b
JungEun Park <jungeunp97@naver.com>
2025-04-19 23:26:25 +0900

    feat: Apply createCompany related structs
    - companyController.ts : createCompanyBodyStruct와 assert하는 것 추가

--- src/controllers/companyController.ts
+++ src/controllers/companyController.ts
@@ -1,8 +1,12 @@
 import { RequestHandler } from 'express';
 import { CreateCompanyDTO } from '../dto/companyDto';
 import * as companyService from '../services/companyService';
+import { assert } from 'superstruct';
+import { createCompanyBodyStruct } from '../structs/companyStruct';
 
 export const createCompany: RequestHandler = async (req, res) => {
+  assert(req.body, createCompanyBodyStruct);
+  // Todo: superstruct가 던지는 에러 잘 잡히는지 확인 필요
   const { companyName, companyCode } = req.body;
   const dto: CreateCompanyDTO = {
     companyName,
