959f726fbc8f2714399c71a73c2ae3a837a86b55
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-04-21 16:18:57 +0900

    feat: contractDocument_DTO_TYPE,contract_DTO_TYPE
    변경 : contractDTO
    생성 : contractDocumentDTO, contractDocumentType

---
+++ src/types/contractType.ts
@@ -0,0 +1,14 @@
+export interface ContractParticipant {
+  id: number;
+  name: string;
+}
+
+export interface ContractCar {
+  id: number;
+  model: string;
+}
+
+export interface Meeting {
+  date: string;
+  alarms: string[];
+}
