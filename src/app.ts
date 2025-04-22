70f1008c9895ac2545d0e6c270dbef57445d5eb2
KiHyun <tjrrl1623@naver.com>
2025-04-22 09:21:40 +0900

    feat: customers app.ts customers router added

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,9 @@ app.use(express.json());
 app.use('/cars', carRoutder);
 app.use('/companies', companyRouter);
 app.use('/users', userRouter);
-<<<<<<< HEAD
-app.use('/customers', customerRouter);
-=======
 app.use('/companies', companyRouter);
 app.use('/users', userRouter);
->>>>>>> 8aa080d49f070b44251bef33fbad01dfdd001565
+app.use('/customers', customerRouter);
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
