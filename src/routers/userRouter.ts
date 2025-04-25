fae860af79dca2f7258a733d7ed0194773ac08c9
JungEun Park <jungeunp97@naver.com>
2025-04-25 17:36:53 +0900

    rebase conflict 수정

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -2,29 +2,23 @@ import express from 'express';
 // Todo: withAsync 가져와서 적용
 // Todo: 인증인가 미들웨어 가져와서 적용
 
-<<<<<<< HEAD
 import {
   createUser,
   getMyInfo,
   updateMyInfo,
   deleteMyAccount,
+  deleteUser,
 } from '../controllers/userController';
-=======
-import { createUser, getMyInfo, updateMyInfo, deleteUser } from '../controllers/userController';
->>>>>>> 9f646c2 (rebase 전 커밋)
 import { verifyAccessToken } from '../middlewares/verifyAccessToken';
 
 export const userRouter = express.Router();
 
 userRouter.post('/', createUser);
-<<<<<<< HEAD
 userRouter
   .route('/me')
   .all(verifyAccessToken)
   .get(getMyInfo)
   .patch(updateMyInfo)
   .delete(deleteMyAccount);
-=======
 userRouter.route('/me').get(verifyAccessToken, getMyInfo).patch(verifyAccessToken, updateMyInfo);
 userRouter.delete('/:id', deleteUser);
->>>>>>> 9f646c2 (rebase 전 커밋)
