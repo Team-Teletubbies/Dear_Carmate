93bf2a2024ddb21bb6d4374e73ed05b7e478dfa9
JungEun Park <jungeunp97@naver.com>
2025-04-30 18:37:38 +0900

    fix: struct 관련 수정
    - pagination 관련 최대 최소값, 길이제한 추가
    - user 관련 struct 에 길이제한 추가, email 및 phoneNumber 제한 추가
    - company 관련 struct 에 길이제한 추가

--- src/structs/commonStruct.ts
+++ src/structs/commonStruct.ts
@@ -1,6 +1,23 @@
-import { coerce, integer, string, object, defaulted, optional, nonempty, Infer } from 'superstruct';
+import {
+  coerce,
+  integer,
+  string,
+  object,
+  defaulted,
+  optional,
+  nonempty,
+  Infer,
+  min,
+  max,
+  size,
+} from 'superstruct';
 
-const integerString = coerce(integer(), string(), (value: string) => parseInt(value));
+const integerString = coerce(min(integer(), 1), string(), (value: string) => parseInt(value));
+
+// pageSize는 너무 크면 서버 터질 것 같아서 max 값도 추가해봤습니다
+const limitedIntegerString = coerce(max(min(integer(), 1), 100), string(), (value: string) =>
+  parseInt(value),
+);
 
 export const IdParamsStruct = object({
   id: integerString,
@@ -10,8 +27,8 @@ export type IdParams = Infer<typeof IdParamsStruct>;
 
 export const PageParamsStruct = object({
   page: defaulted(integerString, 1),
-  pageSize: defaulted(integerString, 8),
-  keyword: optional(nonempty(string())),
+  pageSize: defaulted(limitedIntegerString, 8),
+  keyword: optional(size(string(), 0, 100)),
 });
 
 export type PageParams = Infer<typeof PageParamsStruct>;
