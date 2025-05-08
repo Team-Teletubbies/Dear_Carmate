d23f3366966ca3930bc56cda1c7f2a7de5689805
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-05-08 16:16:35 +0900

    bug:fix_connect_front_uplaodMiddleware
    파일 수정:uploadMiddleware

--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -1,13 +1,17 @@
 import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/contracts/');
   },
   filename: (req, file, cb) => {
-    const savedName = file.originalname;
+    const originalBuffer = Buffer.from(file.originalname, 'latin1');
+    const decodedName = originalBuffer.toString('utf-8');
 
-    cb(null, savedName);
+    const ext = path.extname(decodedName);
+
+    cb(null, decodedName);
   },
 });
 
