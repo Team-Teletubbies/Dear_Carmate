b3af660ffc1a034350a5d99e921a56512a01691d
minso0317 <minso0317@naver.com>
2025-05-12 13:31:00 +0900

    Update redis.ts

--- src/lib/auth/redis.ts
+++ src/lib/auth/redis.ts
@@ -1,3 +1,9 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis(process.env.REDIS_URL!);
+export const redis = new Redis(process.env.REDIS_URL!, {
+  tls: {}, 
+});
+
+redis.on('error', (err) => {
+  console.error('[Redis error]', err.message);
+});
